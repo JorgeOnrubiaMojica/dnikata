6558b6f229595018f37fa8fcda1b99543223fbdc
Jorge Onrubia <jorge.onrubia.mojica@gmail.com>
2021-11-12 21:49:20 +0100

    Refactor and remove unused functions

--- src/main/kotlin/DNI.kt
+++ src/main/kotlin/DNI.kt
@@ -1,38 +1,23 @@
-class DNI(private val value: String, private val VALID_LENGTH: Int = 9) {
+class DNI(private val value: String) {
 
     init {
 
-        val pattern = Regex("^[0-9]{8}[A-Z]$")
-
-        if (!pattern.containsMatchIn(value)){
-            throw IllegalArgumentException()
-        }
+        assertHasValidFormat()
 
         assertLetterIsNotIorO()
 
     }
 
-    private fun assertFirst8CharactersAreADigit() {
-        val first8Char: CharSequence = value.subSequence(0, value.length - 1)
-        if (first8Char.any { ch -> !ch.isDigit() }) {
+    private fun assertHasValidFormat() {
+        val pattern = Regex("^[0-9]{8}[A-Z]$")
+
+        if (!pattern.containsMatchIn(value)) {
             throw IllegalArgumentException()
         }
     }
 
     private fun assertLetterIsNotIorO() {
         if (value.last() == 'I' || value.last() == 'O') {
-            throw IllegalArgumentException()
-        }
-    }
-
-    private fun assertLastCharacterIsLetter() {
-        if (!value.last().isLetter()) {
-            throw IllegalArgumentException()
-        }
-    }
-
-    private fun assertValidLength() {
-        if (value.length != VALID_LENGTH) {
             throw IllegalArgumentException()
         }
     }
